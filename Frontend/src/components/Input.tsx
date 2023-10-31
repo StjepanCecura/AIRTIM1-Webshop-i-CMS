a682965356991b8fce646ccdb1bff80b2f341de8
Nikola Risek <nikolarisek001@gmail.com>
2023-10-31 14:35:04 +0100

    Implemented form data saving.
    Form data is saved in local state using React useState hook.

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -1,12 +1,16 @@
 interface Props {
   type: string
   placeholder: string
+  onChange: (e: any) => void
+  value: string
 }
 
-const Input = ({ type, placeholder }: Props) => {
+const Input = ({ type, placeholder, onChange, value }: Props) => {
   return (
     <div className="w-full">
       <input
+        onChange={onChange}
+        value={value}
         placeholder={placeholder}
         type={type}
         className="bg-tetriary w-full p-3 rounded-lg border-2 border-tetriary focus:border-primary focus:shadow-sm focus:shadow-gray-500"
--- Frontend/src/pages/Register.tsx
+++ Frontend/src/pages/Register.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
 import Button from "../components/Button"
 import Input from "../components/Input"
 
 function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    password: "",
+  })
+
+  const handleChange = (
+    key: keyof typeof formData,
+    value: number | string | boolean
+  ) => {
+    setFormData({
+      ...formData,
+      [key]: value,
+    })
+  }
   return (
     <div className="flex flex-col md:flex-row w-full justify-evenly h-screen items-center">
       <div className="text-[64px] md:flex-1  flex flex-col items-center">
@@ -15,15 +33,40 @@ function Register() {
       </div>
       <div className="md:flex-1 w-full flex flex-col justify-center items-center px-12">
         <div className="w-full md:w-1/2 flex flex-col gap-4">
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Last name" />
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Phone" />
-          <Input type="password" placeholder="Password" />
+          <Input
+            type="text"
+            placeholder="Name"
+            onChange={(e) => handleChange("name", e.target.value)}
+            value={formData.name}
+          />
+          <Input
+            type="text"
+            placeholder="Last name"
+            onChange={(e) => handleChange("lastName", e.target.value)}
+            value={formData.lastName}
+          />
+          <Input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => handleChange("email", e.target.value)}
+            value={formData.email}
+          />
+          <Input
+            type="text"
+            placeholder="Phone"
+            onChange={(e) => handleChange("phone", e.target.value)}
+            value={formData.phone}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => handleChange("password", e.target.value)}
+            value={formData.password}
+          />
           <Button
             text="Create account"
             onClick={() => {
-              console.log("BOK")
+              console.log("BOK -> ", formData)
             }}
           />
         </div>
