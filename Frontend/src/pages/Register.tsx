3090f6812d31483a8ecdcd1144373b859b64e312
Nikola Risek <nikolarisek001@gmail.com>
2023-10-31 13:34:28 +0100

    Made register page title.
    Added primary color to tailwind config file.

--- Frontend/src/pages/Register.tsx
+++ Frontend/src/pages/Register.tsx
@@ -1,5 +1,20 @@
 function Register() {
-  return <div>Register</div>
+  return (
+    <div className="flex flex-col md:flex-row w-full  justify-evenly h-screen items-center">
+      <div className="text-[64px] flex-1  flex flex-col items-center">
+        <div className="flex flex-col font-semibold tracking-wide">
+          <p className="p-0 m-0">Create</p>
+          <p className="p-0 m-0">
+            <span className="text-primary">AiR</span>Express
+          </p>
+          <p className="p-0 m-0">account</p>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        Form
+      </div>
+    </div>
+  )
 }
 
 export default Register
