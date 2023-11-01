71b90236ef12b6f47dc63877124b3371be77d27d
Nikola Risek <nikolarisek001@gmail.com>
2023-11-01 20:26:15 +0100

    Installed and implemented spinner component.

---
+++ Frontend/src/components/Spinner.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Oval } from "react-loader-spinner"
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <Oval
+        height={30}
+        width={30}
+        color="white"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="black"
+        strokeWidth={8}
+        strokeWidthSecondary={8}
+      />
+    </div>
+  )
+}
+
+export default Spinner
