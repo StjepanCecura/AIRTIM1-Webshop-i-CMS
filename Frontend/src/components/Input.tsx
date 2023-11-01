6bf2c8f1b245081faa1fa0e1c050d3c5367c4a2a
Nikola Risek <nikolarisek001@gmail.com>
2023-11-01 21:06:34 +0100

    Improved password input component.
    Users can see and hide their password.

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import EyeSVG from "../assets/eye.svg"
+import EyeCrossedSVG from "../assets/eye-crossed.svg"
 interface Props {
   type: string
   placeholder: string
@@ -10,15 +13,39 @@ const Input = ({ type, placeholder, onChange, value, errorMessage }: Props) => {
   const borderColorClass =
     (errorMessage ?? "") == "" ? "border-tetriary" : "border-red-500"
 
+  const [passwordState, setPasswordState] = useState(type)
+
+  const handlePasswordClick = () => {
+    if (passwordState === "password") setPasswordState("text")
+    if (passwordState === "text") setPasswordState("password")
+  }
+
+  const isPassword = type === "password"
+
   return (
     <div className="w-full">
-      <input
-        onChange={onChange}
-        value={value}
-        placeholder={placeholder}
-        type={type}
-        className={`bg-tetriary w-full p-3 rounded-lg border-2 focus:border-primary focus:shadow-sm focus:shadow-gray-500 ${borderColorClass}`}
-      />
+      <div className="relative">
+        <input
+          onChange={onChange}
+          value={value}
+          placeholder={placeholder}
+          type={isPassword ? passwordState : type}
+          className={`bg-tetriary w-full p-3 pr-10 rounded-lg border-2 focus:border-primary focus:shadow-sm focus:shadow-gray-500 ${borderColorClass}`}
+        />
+        {isPassword ? (
+          <div
+            className="absolute right-3 top-[18px] hover:cursor-pointer"
+            onClick={handlePasswordClick}
+          >
+            {passwordState === "password" ? (
+              <img src={EyeSVG} alt="" className="h-4" />
+            ) : (
+              <img src={EyeCrossedSVG} alt="" className="h-4" />
+            )}
+          </div>
+        ) : null}
+      </div>
+
       {errorMessage ? (
         <p className="text-sm text-red-500">{errorMessage}</p>
       ) : null}
