3e849a6bbc328ef828be117003ea868aac2d82c8
Nikola Risek <nikolarisek001@gmail.com>
2023-11-02 14:08:56 +0100

    Implemented url parameter reading.
    Url will contain user id that will be used to validate user's email.

--- Frontend/src/pages/EmailVerification.tsx
+++ Frontend/src/pages/EmailVerification.tsx
@@ -1,16 +1,45 @@
+import { useEffect, useState } from "react"
 import EmailVerifiedSVG from "../assets/email-verified.svg"
 import Button from "../components/Button"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
+import Spinner from "../components/Spinner"
 
 const EmailVerification = () => {
   const navigate = useNavigate()
+  const params = useParams()
+
+  const [isLoading, setIsLoading] = useState(false)
+
   const handleHomeClick = () => {
     navigate("/")
   }
+
+  const verifyUserEmail = async (id: string) => {
+    setIsLoading(true)
+    console.log("ID -> ", id)
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    if ((params?.id ?? "") !== "") {
+      verifyUserEmail(params?.id as string)
+    }
+    return () => {}
+  }, [params])
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-1 justify-center items-center flex-col gap-7">
+        <Spinner />
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-1 justify-center items-center flex-col gap-7">
       <img src={EmailVerifiedSVG} alt="" className="h-[10rem] ml-4" />
-      <p className="text-[36px] text-center">Email successfully verified!</p>
+      <p className="text-[36px] text-center">Email verified successfully!</p>
       <div className="w-[300px] mt-4">
         <Button text="Home page" onClick={handleHomeClick} />
       </div>
