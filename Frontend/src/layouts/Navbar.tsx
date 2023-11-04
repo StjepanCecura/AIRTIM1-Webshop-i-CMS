378f7c29a57a53c48f30a03e7db2c01ac436bb9d
Nikola Risek <nikolarisek001@gmail.com>
2023-11-04 21:32:45 +0100

    Added home link to navbar.

--- Frontend/src/layouts/Navbar.tsx
+++ Frontend/src/layouts/Navbar.tsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <div className="flex flex-row px-10 py-3">
-      <div className="flex flex-row flex-1">
+      <div className="flex flex-row flex-1 gap-6">
         <Link to="/" className="font-semibold">
           <span className="text-primary">AiR</span>Express
         </Link>
+        <Link to="/">Home</Link>
       </div>
       <div className="flex flex-row">
         <Link to="/login" className="text-primary">
