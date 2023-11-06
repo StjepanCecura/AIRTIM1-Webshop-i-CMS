d596ada78175a69aec40e914577da6958a1dc99b
Nikola Risek <nikolarisek001@gmail.com>
2023-11-06 15:25:19 +0100

    Made NavBar's for mobile and desktop.

--- Frontend/src/layouts/Navbar.tsx
+++ Frontend/src/layouts/Navbar.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom"
 
-const Navbar = () => {
+const NavbarDesktop = () => {
   return (
-    <div className="flex flex-row px-10 py-3">
+    <div className="hidden lg:flex flex-row px-10 py-3">
       <div className="flex flex-row flex-1 gap-6">
         <Link to="/" className="font-semibold">
           <span className="text-primary">AiR</span>Express
@@ -15,6 +15,32 @@ const Navbar = () => {
         </Link>
       </div>
     </div>
+  )
+}
+
+const NavbarMobile = () => {
+  return (
+    <div className="lg:hidden flex flex-row px-10 py-3">
+      <div className="flex flex-row flex-1 gap-6">
+        <Link to="/" className="font-semibold">
+          <span className="text-primary">AiR</span>Express
+        </Link>
+      </div>
+      <div className="flex flex-row">
+        <Link to="/login" className="text-primary">
+          Sign In
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+const Navbar = () => {
+  return (
+    <>
+      <NavbarDesktop />
+      <NavbarMobile />
+    </>
   )
 }
 
