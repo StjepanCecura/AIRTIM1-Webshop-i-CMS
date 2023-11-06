711b42512d5b35cdaf13cf087bd5ab4e107957a1
Nikola Risek <nikolarisek001@gmail.com>
2023-11-06 15:31:54 +0100

    Added hamburger icon for mobile menu.

--- Frontend/src/layouts/Navbar.tsx
+++ Frontend/src/layouts/Navbar.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom"
+import MenuSVG from "../assets/menu.svg"
+import { useState } from "react"
 
 const NavbarDesktop = () => {
   return (
@@ -19,19 +21,29 @@ const NavbarDesktop = () => {
 }
 
 const NavbarMobile = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const handleMenuClick = () => {
+    setMenuOpen((prevstate) => !prevstate)
+  }
   return (
-    <div className="lg:hidden flex flex-row px-10 py-3">
-      <div className="flex flex-row flex-1 gap-6">
-        <Link to="/" className="font-semibold">
-          <span className="text-primary">AiR</span>Express
-        </Link>
+    <>
+      <div className="lg:hidden flex flex-row px-10 py-3">
+        <div className="flex flex-row flex-1 gap-6">
+          <Link to="/" className="font-semibold">
+            <span className="text-primary">AiR</span>Express
+          </Link>
+        </div>
+        <div className="flex flex-row">
+          <div
+            className="flex flex-row items-center hover:cursor-pointer"
+            onClick={handleMenuClick}
+          >
+            <img src={MenuSVG} className="h-4" />
+          </div>
+        </div>
       </div>
-      <div className="flex flex-row">
-        <Link to="/login" className="text-primary">
-          Sign In
-        </Link>
-      </div>
-    </div>
+      {menuOpen ? <div>Hello this is menu</div> : null}
+    </>
   )
 }
 
