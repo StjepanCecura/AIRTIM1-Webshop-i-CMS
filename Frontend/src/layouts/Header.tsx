de637571c16cf3f1e5f9abfc103697a15e0291c7
Nikola Risek <nikolarisek001@gmail.com>
2023-11-20 13:23:35 +0100

    Made basic header layout that shows data from contentful.

--- Frontend/src/layouts/Header.tsx
+++ Frontend/src/layouts/Header.tsx
@@ -1,7 +1,13 @@
-const Header = () => {
+import { IHeader } from "../types/header.type"
+
+const Header = ({ headerData }: { headerData: IHeader }) => {
   return (
     <div className="w-full bg-primary flex justify-center items-center flex-col">
-      <h1>Header title</h1>
+      <h1>{headerData?.title}</h1>
+      <img
+        src={`https:${headerData?.image?.url}`}
+        alt={headerData?.image?.description}
+      />
     </div>
   )
 }
