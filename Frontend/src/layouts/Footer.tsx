31611e0e0c764c06ff39ae0282828104d158efde
Nikola Risek <nikolarisek001@gmail.com>
2023-11-20 13:23:12 +0100

    Made basic footer layout that shows data from contentful.

--- Frontend/src/layouts/Footer.tsx
+++ Frontend/src/layouts/Footer.tsx
@@ -1,7 +1,19 @@
-const Footer = () => {
+import { IFooter } from "../types/footer.type"
+
+const Footer = ({ footerData }: { footerData: IFooter }) => {
   return (
-    <div className="w-full bg-primary flex justify-center items-center">
-      Footer
+    <div className="w-full bg-primary flex justify-center items-center flex-col">
+      <p>
+        <a href={footerData?.email}>{footerData?.email}</a>
+      </p>
+      <p>
+        <a href={`tel:${footerData?.phoneNumber}`}>{footerData?.phoneNumber}</a>
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: footerData?.longText }} />
+      <img
+        src={`https:${footerData?.image?.url}`}
+        alt={footerData?.image?.description}
+      />
     </div>
   )
 }
