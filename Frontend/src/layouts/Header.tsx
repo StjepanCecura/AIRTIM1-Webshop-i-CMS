4cad862828c1add267b6af5403a746659d4e3a2f
Nikola Risek <nikolarisek001@gmail.com>
2023-11-20 15:41:19 +0100

    Added description to header.

--- Frontend/src/layouts/Header.tsx
+++ Frontend/src/layouts/Header.tsx
@@ -2,12 +2,22 @@ import { IHeader } from "../types/header.type"
 
 const Header = ({ headerData }: { headerData: IHeader }) => {
   return (
-    <div className="w-full bg-primary flex justify-center items-center flex-col">
-      <h1>{headerData?.title}</h1>
-      <img
-        src={`https:${headerData?.image?.url}`}
-        alt={headerData?.image?.description}
-      />
+    <div className="w-full flex flex-col md:flex-row justify-center items-center  bg-gradient-to-r from-slate-600 to-black gap-8">
+      {/* <div className="bg-black opacity-60 absolute inset-0 flex justify-center items-center"></div> */}
+      <div className="flex-1">
+        <img
+          src={`https:${headerData?.image?.url}`}
+          alt={headerData?.image?.description}
+        />
+      </div>
+      <div className="flex w-full justify-center items-center flex-col flex-1 gap-8 pb-12 px-10 md:pb-0">
+        <h1 className=" text-white font-bold text-[48px] md:text-[64px] ">
+          {headerData?.title}
+        </h1>
+        <p className="text-white text-center text-[18px]">
+          {headerData?.description}
+        </p>
+      </div>
     </div>
   )
 }
--- Frontend/src/types/header.type.tsx
+++ Frontend/src/types/header.type.tsx
@@ -1,5 +1,6 @@
 export interface IHeader {
   title: string
+  description: string
   image: {
     title: string
     description: string
