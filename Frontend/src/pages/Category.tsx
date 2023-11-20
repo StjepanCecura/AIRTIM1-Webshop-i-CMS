b65a77b7f57943397dbb9213da2173fc625c8790
Nikola Risek <nikolarisek001@gmail.com>
2023-11-20 12:51:46 +0100

    Added API call for page details.
    Based on slug.

--- Frontend/src/pages/Category.tsx
+++ Frontend/src/pages/Category.tsx
@@ -2,6 +2,7 @@ import Header from "../layouts/Header"
 import Footer from "../layouts/Footer"
 
 const Category = () => {
+  // TODO: dont use Category for category pages, use defaultPage instead. You can determine if you are on /c/slug by using useLocation hook.
   return (
     <div className="flex flex-col h-[calc(100vh-52px)]">
       <Header />
--- Frontend/src/pages/DefaultPage.tsx
+++ Frontend/src/pages/DefaultPage.tsx
@@ -1,7 +1,42 @@
 import Header from "../layouts/Header"
 import Footer from "../layouts/Footer"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { API_URL } from "../constants"
+import Spinner from "../components/Spinner"
 
 const DefaultPage = () => {
+  const { slug } = useParams()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getPageBySlug = async () => {
+    setIsLoading(true)
+    await axios
+      .get(`${API_URL}/product/getPage?slug=${slug}`)
+      .then((res) => {
+        console.log("RES PAGE-> ", res)
+      })
+      .catch((err) => {
+        console.log("ERROR PAGE -> ", err)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    if (slug != "") getPageBySlug()
+    return () => {}
+  }, [slug])
+
+  if (isLoading)
+    return (
+      <div className="flex flex-1 justify-center items-center">
+        <Spinner />
+      </div>
+    )
+
   return (
     <div className="flex flex-col h-[calc(100vh-52px)]">
       <Header />
