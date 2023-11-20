e0878540e3453aaa6c99521ec216f6fa7b790c1f
Nikola Risek <nikolarisek001@gmail.com>
2023-11-20 16:09:17 +0100

    Implemented footer based on content from Contentful.

--- Frontend/src/layouts/Footer.tsx
+++ Frontend/src/layouts/Footer.tsx
@@ -2,18 +2,27 @@ import { IFooter } from "../types/footer.type"
 
 const Footer = ({ footerData }: { footerData: IFooter }) => {
   return (
-    <div className="w-full bg-primary flex justify-center items-center flex-col">
-      <p>
-        <a href={footerData?.email}>{footerData?.email}</a>
-      </p>
-      <p>
-        <a href={`tel:${footerData?.phoneNumber}`}>{footerData?.phoneNumber}</a>
-      </p>
-      <div dangerouslySetInnerHTML={{ __html: footerData?.longText }} />
+    <div className="w-full bg-primary flex justify-center items-center flex-col px-8 md:px-40 py-8 text-slate-200">
       <img
+        width={400}
         src={`https:${footerData?.image?.url}`}
         alt={footerData?.image?.description}
       />
+      <div
+        className="text-center w-[700px] pb-5"
+        dangerouslySetInnerHTML={{ __html: footerData?.longText }}
+      />
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <p>
+          <a href={`mailto:${footerData?.email}`}>{footerData?.email}</a>
+        </p>
+        <p>
+          <a href={`tel:${footerData?.phoneNumber}`}>
+            {footerData?.phoneNumber}
+          </a>
+        </p>
+        <p>&copy; AiRExpress {new Date().getFullYear()}</p>
+      </div>
     </div>
   )
 }
