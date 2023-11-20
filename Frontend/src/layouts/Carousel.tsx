784efccd86650cd2402c0375412976ca75e28753
Nikola Risek <nikolarisek001@gmail.com>
2023-11-20 13:22:52 +0100

    Made Carousel layout.
    It is part of default page.

---
+++ Frontend/src/layouts/Carousel.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { ICarousel } from "../types/carousel.type"
+import Spinner from "../components/Spinner"
+
+const Carousel = ({ carouselData }: { carouselData: ICarousel }) => {
+  return <div>Carousel</div>
+}
+
+export default Carousel
