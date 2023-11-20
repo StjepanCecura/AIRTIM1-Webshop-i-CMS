159ba1992138d1ccffa2b26a700e7508d4a99919
Nikola Risek <nikolarisek001@gmail.com>
2023-11-20 15:40:49 +0100

    Made carousel smaller.

--- Frontend/src/layouts/Carousel.tsx
+++ Frontend/src/layouts/Carousel.tsx
@@ -5,7 +5,7 @@ import { Carousel } from "react-responsive-carousel"
 const CarouselLayout = ({ carouselData }: { carouselData: ICarousel }) => {
   console.log(carouselData)
   return (
-    <div className="flex justify-center items-center w-full py-5 md:px-60 md:py-5">
+    <div className="flex justify-center items-center w-full py-5 md:px-80 md:py-5 bg-tetriary">
       <Carousel
         autoPlay
         interval={3000}
