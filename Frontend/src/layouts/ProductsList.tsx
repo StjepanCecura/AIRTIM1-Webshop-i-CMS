568946da8f4cd661396e02a075a7932ea82c5daf
Nikola Risek <nikolarisek001@gmail.com>
2023-11-27 14:59:01 +0100

    Edited ProductsList component.

--- Frontend/src/layouts/ProductsList.tsx
+++ Frontend/src/layouts/ProductsList.tsx
@@ -1,5 +1,14 @@
+import ProductCard from "../components/ProductCard"
+
 const ProductsList = () => {
-  return <div className="w-full h-full px-8">ProductsList</div>
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  return (
+    <div className="w-full h-full px-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      {arr.map((item, index) => {
+        return <ProductCard key={index} id={item} />
+      })}
+    </div>
+  )
 }
 
 export default ProductsList
