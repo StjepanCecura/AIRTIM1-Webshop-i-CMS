859c30fad1c567a2e57634d08c3300e8ed641d54
Nikola Risek <nikolarisek001@gmail.com>
2023-11-29 17:54:59 +0100

    Created new carousel component for product details page.

--- Frontend/src/layouts/ProductCarousel.tsx
+++ Frontend/src/layouts/ProductCarousel.tsx
@@ -1,11 +1,11 @@
-import { IProduct } from "../interfaces/product.interface"
 import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
 import { Carousel } from "react-responsive-carousel"
+import { IProductDetails } from "../interfaces/productDetails.interface"
 
 const ProductCarousel = ({
   carouselProduct,
 }: {
-  carouselProduct: IProduct
+  carouselProduct: IProductDetails
 }) => {
   return (
     <div>
@@ -17,13 +17,13 @@ const ProductCarousel = ({
         swipeScrollTolerance={5}
         centerSlidePercentage={70}
       >
-        {carouselProduct.images.map((image) => {
+        {carouselProduct?.currentVariant?.images.map((image) => {
           return (
-            <div key={image.url} className="max-h-[600px]">
+            <div key={image?.url} className="max-h-[600px]">
               {/* <img src={`https:${image.url}`} /> */}
               <img
                 className="max-h-[600px] object-contain"
-                src={`${image.url}`}
+                src={`${image?.url}`}
               />
               {/* <p className="legend">{carouselProduct.name}</p> */}
             </div>
