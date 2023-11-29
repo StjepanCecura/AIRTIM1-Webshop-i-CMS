276a8d57717488372a462a8dae23f69623a8acaf
Nikola Risek <nikolarisek001@gmail.com>
2023-11-29 11:16:00 +0100

    Implemented basic product page template.

--- Frontend/src/pages/Product.tsx
+++ Frontend/src/pages/Product.tsx
@@ -1,9 +1,26 @@
 import { useParams } from "react-router-dom"
+import CarouselLayout from "../layouts/Carousel"
+import { ICarousel } from "../interfaces/carousel.interface"
 const Product = () => {
+  const bok: ICarousel = {
+    images: [
+      {
+        title: "Image 1",
+        description: "Image 1 Desc",
+        url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/d706b6dd-32a7-4ed9-81c9-c0808624807c/dunk-low-retro-shoes-JH36z5.png",
+      },
+    ],
+  }
+
   const { productKey, variantKey } = useParams()
   return (
-    <div>
-      Product {productKey} {variantKey}
+    <div className="flex flex-row gap-8 px-8 py-8">
+      <div style={{ flex: 6 }}>
+        <CarouselLayout carouselData={bok} />
+      </div>
+      <div style={{ flex: 5 }}>
+        Product {productKey} {variantKey}
+      </div>
     </div>
   )
 }
