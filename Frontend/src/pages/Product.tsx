95b9ab2c786c5ba13c305ba4e723857aca9dbfe2
Nikola Risek <nikolarisek001@gmail.com>
2023-11-29 10:41:46 +0100

    Display product key and variant key.
    For test purposes.

--- Frontend/src/pages/Product.tsx
+++ Frontend/src/pages/Product.tsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom"
 const Product = () => {
   const { productKey, variantKey } = useParams()
-  return <div>Product</div>
+  return (
+    <div>
+      Product {productKey} {variantKey}
+    </div>
+  )
 }
 
 export default Product
