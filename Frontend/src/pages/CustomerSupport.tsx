bbf67fa5d3e2102b2cc3cffbf70c9da338c25ee3
Nikola Risek <nikolarisek001@gmail.com>
2024-01-15 16:24:33 +0100

    Added method for sending support message.

--- Frontend/src/pages/CustomerSupport.tsx
+++ Frontend/src/pages/CustomerSupport.tsx
@@ -2,6 +2,9 @@ import { useState } from "react"
 import Button from "../components/Button"
 import Input from "../components/Input"
 import Spinner from "../components/Spinner"
+import { toast } from "react-toastify"
+import axios from "axios"
+import { API_URL } from "../constants"
 
 const CustomerSupport = () => {
   const [loadingStack, setLoadingStack] = useState<number[]>([])
@@ -28,7 +31,36 @@ const CustomerSupport = () => {
     })
   }
 
-  const handleSendClick = () => {}
+  const sendMessage = async () => {
+    startLoading()
+    await axios
+      .post(`${API_URL}/customer/support`, {
+        title: messageData.title,
+        body: messageData.body,
+      })
+      .then((res) => {
+        if (res?.status == 200) {
+          if (res?.data?.success == true) toast("Message sent successfully.")
+        }
+        if (res?.data?.error) {
+          toast.error("Error while sending message. Please try again later.")
+        }
+      })
+      .catch((err) => {
+        toast.error("Error while sending message. Please try again later.")
+      })
+      .finally(() => {
+        stopLoading()
+      })
+  }
+
+  const handleSendClick = () => {
+    if (messageData.title == "" || messageData.body == "") {
+      toast("Please enter all data.")
+    } else {
+      //sendMessage()
+    }
+  }
 
   if (loadingStack.length > 0) {
     return (
@@ -52,6 +84,18 @@ const CustomerSupport = () => {
               placeholder="Title"
               onChange={(e) => handleChange("title", e.target.value)}
               value={messageData.title}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <div className="flex flex-col justify-start w-full">
+            <p>Message:</p>
+            <textarea
+              rows={10}
+              onChange={(e) => handleChange("body", e.target.value)}
+              value={messageData.body}
+              placeholder="Message"
+              className="bg-tetriary w-full p-3 rounded-lg border-2 focus:border-primary focus:shadow-sm focus:shadow-gray-500 border-tetriary"
             />
           </div>
         </div>
