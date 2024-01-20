cfb8bd2a617af38ad738319c8a1dc8d32bfa98f2
Nikola Risek <nikolarisek001@gmail.com>
2024-01-20 10:06:45 +0100

    Made payment status pages.

--- Frontend/src/pages/PaymentError.tsx
+++ Frontend/src/pages/PaymentError.tsx
@@ -1,5 +1,25 @@
+import EmailVerifiedSVG from "../assets/email-verified.svg"
+import Button from "../components/Button"
+import { useNavigate } from "react-router-dom"
+
 const PaymentError = () => {
-  return <div>PaymentError</div>
+  const navigate = useNavigate()
+
+  const handleHomeClick = () => {
+    navigate("/")
+  }
+
+  return (
+    <div className="flex flex-1 justify-center items-center flex-col gap-7">
+      <img src={EmailVerifiedSVG} alt="" className="h-[10rem] ml-4" />
+      <p className="text-[36px] text-center">Payment Error</p>
+      <p className="text-[20px] text-center">Error while processing payment!</p>
+
+      <div className="w-[300px] mt-4">
+        <Button text="Home page" onClick={handleHomeClick} />
+      </div>
+    </div>
+  )
 }
 
 export default PaymentError
--- Frontend/src/pages/PaymentSuccess.tsx
+++ Frontend/src/pages/PaymentSuccess.tsx
@@ -1,5 +1,25 @@
+import EmailVerifiedSVG from "../assets/email-verified.svg"
+import Button from "../components/Button"
+import { useNavigate } from "react-router-dom"
+
 const PaymentSuccess = () => {
-  return <div>PaymentSuccess</div>
+  const navigate = useNavigate()
+
+  const handleHomeClick = () => {
+    navigate("/")
+  }
+
+  return (
+    <div className="flex flex-1 justify-center items-center flex-col gap-7">
+      <img src={EmailVerifiedSVG} alt="" className="h-[10rem] ml-4" />
+      <p className="text-[36px] text-center">Payment Successful</p>
+      <p className="text-[20px] text-center">Thank you for shopping with us!</p>
+
+      <div className="w-[300px] mt-4">
+        <Button text="Continue shopping" onClick={handleHomeClick} />
+      </div>
+    </div>
+  )
 }
 
 export default PaymentSuccess
